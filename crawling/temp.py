251249c7b2300c19a02dbdc0d385169cb689bb73
juhyun9 <96998861+juhyun9@users.noreply.github.com>
2023-05-01 21:02:40 +0900

    df 구조 재설계
    민성님 작업 편한 형태로 바꿨습니다.
    
    crawling.csv 파일 바로 열어보세용

--- crawling/temp.py
+++ crawling/temp.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 
 #필요한 모듈
@@ -23,10 +24,9 @@ options = webdriver.ChromeOptions()
 import warnings 
 warnings.filterwarnings('ignore')
 
-global youtuber_pd
 youtuber_pd=pd.DataFrame()
-global comment_final
 comment_finals=[]
+video_num=2
 
 #크롬드라이버 버전맞춰 자동 다운 및 시작
 try:
@@ -46,8 +46,6 @@ try:
             driver = webdriver.Chrome(options=options)
 except:
     print('크롬드라이버 자동설치 실패')
-
-#2030유튜버 랭킹 순위 알 코드
 
 
 #그 유튜브 계정에 가서 관련 영상들 링크 크롤링
@@ -82,6 +80,7 @@ def takeContents(youtuber):
     soup = BeautifulSoup(html_source, 'html.parser')
     global contentlist
     contentlist=[]
+    global video_num
     number=0
     for href in soup.find_all("div", class_='style-scope ytd-rich-item-renderer'):
         number+=1
@@ -90,16 +89,16 @@ def takeContents(youtuber):
         print(number)
         print(hr)
         takeComments(youtuber,hr,number)
-        if number == 10:
+        global video_num
+        if number == video_num:
             break
     
     #contents_data = {'contents': contentlist}
     #Ycontents_pd = pd.DataFrame(contents_data)
-    
-    
-def takeComments(youtuber,content,number):
-    #유튜브 불러오기,유튜버의 영상 1개
-    url = 'https://www.youtube.com'+ str(content)
+
+#그 유튜브 계정에 가서 관련 영상들 링크 크롤링
+def takeComments(youtuber,hr,number):
+    url = 'https://www.youtube.com'+ str(hr)
     print(url)
     wb = Workbook(write_only=True)
     ws = wb.create_sheet()
@@ -146,47 +145,43 @@ def takeComments(youtuber,content,number):
         temp_comment = temp_comment.replace('\t', '')
         temp_comment = temp_comment.replace('    ', '')
         comment_final.append(temp_comment) # 댓글 내용
-        youtuber_final.append(youtuber)
     
-    if content == contentlist[0]:
-        global list1
-        list1 = comment_final
-    elif content == contentlist[1]:
-        global list2
-        list2 = comment_final
-    elif content == contentlist[2]:
-        global list3
-        list3 = comment_final
-    elif content == contentlist[3]:
-        global list4
-        list4 = comment_final
-    elif content == contentlist[4]:
-        global list5
-        list5 = comment_final
-    elif content == contentlist[5]:
-        global list6
-        list6 = comment_final
-    elif content == contentlist[6]:
-        global list7
-        list7 = comment_final
-    elif content == contentlist[7]:
-        global list8
-        list8 = comment_final
-    elif content == contentlist[8]:
-        global list9
-        list9 = comment_final
-    elif content == contentlist[9]:
-        global list10
-        list10 = comment_final
+    data(youtuber,number,comment_final)
+    
+    
+def data(youtuber,number, comment_final):
+    for j in comment_final:
+        global comment_finals
+        comment_finals.append(j)
+    if number == video_num:
+        '''
+        global df
+        df={'youtuber'+str(number):youtuber,'comments':comment_finals}
+        '''
+        global comm
+        comm = ','.join(comment_finals)
+        print(comm)
+        print(youtuber)
 
-    print('comments crawling done')
-    
+        global youtuber_pd
+        youtuber_pd["youtuber"] = [youtuber]
+        youtuber_pd["comments"] = [comm]
+
+def data(youtuber,number, comment_final):
+    for j in comment_final:
+        global comment_finals
+        comment_finals.append(j)
     if number == 10:
-        df={'youtuber':youtuber_final,'1':list1,'2':list2,'3':list3,'4':list4,'5':list5,'6':list6,'7':list7,'8':list8,'9':list9,'10':list10}
+        df={'youtuber'+str(number):youtuber,'comments':comment_finals}
         temperature_df = pd.DataFrame.from_dict(df, orient='index')
         global youtuber_pd
         youtuber_pd=pd.concat([youtuber_pd,temperature_df], axis=1)
-        return youtuber_pd
+        
+        
+youtuberlist=['@shyfilms','@wonchan','@depublik']
+
+for i in youtuberlist:
+    takeContents(i)
 
 def youtuberinput():
     inputyoutuber=str()
@@ -206,5 +201,6 @@ youtuberlistN=[]
 youtuberinput()
 for i in youtuberlist:
     takeContents(i)
-youtuber_pd.transpose()
-youtuber_pd.to_excel('result.xlsx')
+
+
+youtuber_pd.to_csv('crawling.csv', sep=',', encoding='utf-8')
