329dd363960827999cb118b9be89ee104d31f86f
gomsung <126758468+gomsung@users.noreply.github.com>
2023-05-03 15:08:40 +0900

    Update keyword2.py

--- keyword/keyword2.py
+++ keyword/keyword2.py
@@ -17,13 +17,18 @@ stop_words = stopwords.words('korean')
 stop_words.extend(['을', '를', '이', '가', '은', '는', '에', '의', '로'])
 
 
-#이모지 제거
-emoji_pattern = re.compile("["u"\U0001F600-\U0001F64F"
+comments_daa = []
+  #이모지 제거
+  emoji_pattern = re.compile("["u"\U0001F600-\U0001F64F"
         u"\U0001F300-\U0001F5FF"
         u"\U0001F680-\U0001F6FF"
         u"\U0001F1E0-\U0001F1FF"
                            "]+", flags = re.UNICODE)
-
+    data_str = ' '.join(map(str,df[name].tolist()))
+    data_str = emoji_pattern.sub(r'', data_str)
+    data_str = re.sub("[^가-힣ㄱ-ㅎㅏ-ㅣ\\s]", "" ,data_str)    
+    comments_data.append(data_str) 
+  
 okt = Okt()
 
 ## 벡터화
@@ -35,5 +40,7 @@ var_data_str = [' '.join(map(str, lst))for lst in var_data]
 vectors = vectorizer.fit_transform([emoji_pattern.sub(r'', text)) for text in var_data_str]).toarray()
 
 df_vectors = pd.DataFrame(vectors, columns = vectorizer.get_feature_names_out(), index = df.columns)
-#df_vectors.index.name = 'youtuber'
-df_vectors.columns.name = 'comments'
+df_vectors.to_csv('C:/df_vector.csv', index = False)
+
+comments_df = pd.DataFrame({'comments': comments_data})
+comments_df.to_csv('C:/comments_data.csv', index=False)
