dadcf7850dff01eddaf9f3e07a1b0f5b36a7b6ba
juhyun9 <990001@stu.jejunu.ac.kr>
2023-05-11 00:35:48 +0900

    벡터화 데이터프레임 통합과정
    각 행별 계산중

---
+++ keyword/주현2.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from konlpy.tag import Okt
+from nltk.corpus import stopwords
+
+
+df = pd.read_csv('sourc/crawling.csv')
+
+
+
+var_data = [df[comments].tolist() for comments in df.columns]
+
+  #이모지 제거
+emoji_pattern = re.compile("["u"\U0001F600-\U0001F64F"
+        u"\U0001F300-\U0001F5FF"
+        u"\U0001F680-\U0001F6FF"
+        u"\U0001F1E0-\U0001F1FF"
+                           "]+", flags = re.UNICODE)
+
+comments_data = []
+data_str = ' '.join(map(str,df['comments'].tolist()))
+data_str = emoji_pattern.sub(r'', data_str)
+data_str = re.sub("[^가-힣ㄱ-ㅎㅏ-ㅣ\\s]", "" ,data_str)
+data_str = re.sub('[^\w\d\s]','',data_str)
+
+#불용어 제거
+import pickle
+'''
+#불용어 목록 입력
+bull = []
+inputword=str()
+while inputword != 'no':
+    inputword = input()
+    bull.append(inputword)
+bull.remove('no')
+'''
+#불용어 리스트 저장
+with open('sourc/bull.pkl','wb') as f :
+    pickle.dump(bull,f)
+#불용어 리스트 불러오기
+with open ('sourc/bull.pkl','rb') as f: 
+    bullword = pickle.load(f)
+  
+okt = Okt()
+stop_words = stopwords.words('korean')
+stop_words.extend(bullword)
+stop_words = set(stop_words.split(''))
+word_tokens = okt.morphs(data_str)
+result = [word for word in word_tokens if not word in stop_words]
+
+comments_data.append(data_str)
+
+df['clean_com'] = pd.Series(comments_data)
+
+data_str.split()
+df['clean_word'] = pd.series(data_str)
+
+## 벡터화
+vectorizer = TfidfVectorizer(stop_words = stop_words)
+
+#리스트 문자열 변환
+var_data_str = [' '.join(map(str, lst))for lst in var_data]
+
+vectors = vectorizer.fit_transform([(emoji_pattern.sub(r'', text)) for text in var_data_str]).toarray()
+
+df_vectors = pd.DataFrame(vectors, columns = vectorizer.get_feature_names_out(), index = df.columns)
+df_vectors.to_csv('sourc/df_vector_ju.csv', index = False)
+
+comments_df = pd.DataFrame({'comments': comments_data})
+comments_df.to_csv('sourc/comments_data_ju.csv', index=False)
